2f8b5298e7e6b4a1cee84b16097af73337b9d877
ManoelOscar35 <manoeloscar557@gmail.com>
2025-04-22 17:05:55 -0300

    feat[gerencial]: header

--- src/app/gerencial/components/main/components/header/header.component.ts
+++ src/app/gerencial/components/main/components/header/header.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../../../material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MaterialModule,
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- src/app/gerencial/components/main/main.component.ts
+++ src/app/gerencial/components/main/main.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+const angularModules = [CommonModule, RouterModule];
+const components = [ HeaderComponent ];
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [...angularModules, ...components],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
