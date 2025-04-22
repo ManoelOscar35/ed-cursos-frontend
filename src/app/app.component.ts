8246982fb2d0928f345979093fe78e7f7da834a3
ManoelOscar35 <manoeloscar557@gmail.com>
2025-04-22 11:54:50 -0300

    chore: initial project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from './material.module';
+
+const modules = [MaterialModule];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ...modules],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
